fa98a7aadeace1d619388328c51b16d942e31892
Ashish Soni <ashish.manju.soni@gmail.com>
2026-02-10 21:50:24 +0530

    cocktail shacker sort

---
+++ testcase/cocktail_shacker_sort_test.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<tm_sort>
+#include<vector>
+#include<array>
+int compare(int left,int right)
+{
+return left-right;
+}
+
+int main()
+{
+std::vector<int> v{95,65,-12,-6,0,10,1,123,1000,-768,34,234,5435,636,234,6090,9881,45524,24354,13218,3251,65431};
+algo::cocktail_shacker_sort(v.begin(),v.end(),compare);
+for(int i:v) std::cout<<i<<" ";
+std::cout<<std::endl;
+std::cout<<"-----------------"<<std::endl;
+std::array<int,23> a={95,65,-12,-6,0,10,1,123,1000,-768,-8989,454,-898,54634,32684,3216,13232,1100,5456,11210,34534,1356432,3524};
+algo::cocktail_shacker_sort(a.begin(),a.end(),compare);
+for(const int i:a) std::cout<<i<<" ";
+std::cout<<std::endl;
+return 0;
+}
